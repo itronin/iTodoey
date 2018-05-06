677d1f6186aa1f5b5836d37e96aad2bd4be60a10
Igor Tro <igortro@igors-macbook-air.local>
2018-05-06 21:58:55 +0300

    Initial Commit

---
+++ iTodoey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iTodoey
+//
+//  Created by Igor Tro on 06/05/2018.
+//  Copyright © 2018 Igor Tro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
